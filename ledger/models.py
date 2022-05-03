f25a62a9056c14d583a22e99869c530c4b25ee25
GuilhermeBaldo <guilhermebaldocarlos@gmail.com>
2022-05-03 16:20:42 -0300

    models created and admin page started

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel
 
-# Create your models here.
+
+class Type(SafeDeleteModel):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    deleted_at = models.DateTimeField(null=True)
+
+    name = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class Account(SafeDeleteModel):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    deleted_at = models.DateTimeField(null=True)
+
+    name = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(SafeDeleteModel):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    deleted_at = models.DateTimeField(null=True)
+
+    date = models.DateTimeField()
+
+    account = models.ForeignKey(Account, related_name='transactions', on_delete=models.DO_NOTHING)
+
+    type = models.ForeignKey(Type, related_name='transactions', on_delete=models.DO_NOTHING)
+
+    value = models.FloatField(default=0)
+
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.date} Account: {self.account}, Value: {self.value}, Type: {self.type}, Description: {self.description}"
